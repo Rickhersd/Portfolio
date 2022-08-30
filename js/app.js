9e6231953c76fdfefec805f9a779aded62bbe8a4
Ricardo Hernandez <rickhersd2002@gmail.com>
2022-08-30 02:02:58 -0400

    insersectionObserver

--- js/app.js
+++ js/app.js
@@ -5,13 +5,34 @@ function btnDropdown () {
 window.onclick = (event) => {
     if (!event.target.matches('.dropdown')) {
         let el = document.getElementsByClassName("dropdown");
-        console.log (el.length);
         for(let i = 0; i < el.length; i++){
             if (el[i].classList.contains("show")){
                 el[i].classList.remove("show");
             }
         }
     }
+}
 
+if(!!window.IntersectionObserver){
+    
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    }
 
+    let observer = new IntersectionObserver((entries, observer) => 
+    {       
+        entries.forEach((entry) => 
+        {
+            if (entry.isIntersecting){ 
+                console.log(entry);
+                entry.target.classList.add("extra");
+                observer.unobserve(entry.target);
+            };
+        });
+    }, options); 
+
+    boxElement = document.querySelector(".block");
+    observer.observe(boxElement);
 }
